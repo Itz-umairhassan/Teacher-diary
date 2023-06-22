05f8d2138a0c7efd8b444439994b7b98a3714fee
umair hassan <umairhassangondal3@gmail.com>
2023-06-22 18:24:34 +0500

    student db + student insertion

--- app/src/main/java/com/example/hifzdiary/AddStudent.java
+++ app/src/main/java/com/example/hifzdiary/AddStudent.java
@@ -14,12 +14,15 @@ public class AddStudent extends AppCompatActivity {
     EditText studentclass;
     EditText studentroll;
     Button add_student;
+    DBHandler studentdb;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_add_student);
 
         try {
+            // create a new object of db hanlder class for student...
+            studentdb=new DBHandler(AddStudent.this);
             add_student = findViewById(R.id.addStudent);
             studentname = findViewById(R.id.studentName);
             studentroll = findViewById(R.id.studentRoll);
@@ -28,8 +31,21 @@ public class AddStudent extends AppCompatActivity {
             add_student.setOnClickListener(new View.OnClickListener() {
                 @Override
                 public void onClick(View view) {
+                    String student_name=studentname.getText().toString();
+                    String student_roll=studentroll.getText().toString();
+                    String student_class=studentclass.getText().toString();
 
+                    if(student_class.isEmpty() || student_roll.isEmpty() || student_name.isEmpty()){
+                        Toast.makeText(AddStudent.this, "Fields are empty", Toast.LENGTH_SHORT).show();
 
+                    }else{
+                        studentdb.addNewStudent(student_name,student_roll,student_class);
+
+                        Toast.makeText(AddStudent.this, "Student "+student_name+" is added ", Toast.LENGTH_SHORT).show();
+                        studentclass.setText("");
+                        studentname.setText("");
+                        studentroll.setText("");
+                    }
                 }
             });
         }catch(Exception ex){
